4b76dceeff0bf972f4555eb87d7595d30e147442
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2025-09-16 16:48:47 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
+use App\Http\Controllers\AppliedProjectContorller;
+use App\Http\Controllers\ProjectContorller;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    return Inertia::render('Main/Page');
+})->name('main-page');
+
+Route::middleware('auth')->group(
+    function () {
+        Route::get('/projects', [ProjectContorller::class, 'index']);
+        Route::get('/projects/create', [ProjectContorller::class, 'create']);
+        Route::get('/poejects/{id}', [ProjectContorller::class, 'show']);
+        Route::post('/projects', [ProjectContorller::class, 'store']);
+        Route::get('/projects/{id}/edit', [ProjectContorller::class, 'edit']);
+        Route::put('/projects/{id}', [ProjectContorller::class, 'update']);
+
+        Route::get('/applied-projects', [AppliedProjectContorller::class, 'index']);
+    }
+);
+
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -13,5 +30,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/admin.php';
+require __DIR__ . '/auth.php';
